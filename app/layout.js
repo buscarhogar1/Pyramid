8c418746bedb0ee56f9dfb1c11545e1bdfbdcd43
Lucrezia Lafratta <lomarcer@gmail.com>
2026-07-18 22:04:32 +0200

    Move privacy site to hosting root

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Privacy Policy | Pyramid - Drinking Game",
+  description: "Privacy policy for Pyramid - Drinking Game.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
